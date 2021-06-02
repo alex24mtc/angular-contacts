b708247f761da535f3fedb229a1cdb24982aced4
alex24mtc <alex25mtc@gmail.com>
2021-06-02 13:41:12 +0200

    commit 3

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -15,6 +15,7 @@ interface Contact {
 })
 export class ContactListComponent implements OnInit {
   contacts: Object[];
+
   newContact: Contact={
     name: "",
     email: "",
@@ -32,6 +33,28 @@ export class ContactListComponent implements OnInit {
 
   addContact(){
     // add contact to contacts list
+
+    //primero se pone la imagen antes que el push, sino no se verá
+
+    this.newContact.image = 'https://images.unsplash.com/photo-1616578853146-cb0b5e732b83?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=498&q=80';
+
+    let addContact = {...this.newContact};
+    this.contacts.push(addContact);
+
     // clear inputs
+
+    this.newContact.image=null;
+    this.newContact.name=null;
+    this.newContact.email=null;
+    this.newContact.phoneNumber=null;
+
+    console.log('Nuevo Contacto creado',this.newContact);
   }
+
+  deleteContact(){
+   this.contacts.pop();
+    
+  }
+
+
 }
